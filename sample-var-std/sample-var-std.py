ab4162b4b5b26cbff3411a0788fe72f5e717ad28
Khoa Thai Minh <149080799+minhkhoa23@users.noreply.github.com>
2026-05-14 12:22:30 +0700

    Add solution for sample-var-std

---
+++ sample-var-std/sample-var-std.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def sample_var_std(x):
+    """
+    Compute sample variance and standard deviation.
+    """
+    # Write code here
+    n = len(x)
+    mean_x = np.mean(x)
+    var = np.sum((x - mean_x) ** 2) / (n - 1)
+    std = np.sqrt(var)
+
+    return var, std
