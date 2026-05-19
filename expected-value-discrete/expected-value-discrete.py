1a4b32e9a13c7fe06628769dfc5ccc58aa1b5cf9
Khoa Thai Minh <149080799+minhkhoa23@users.noreply.github.com>
2026-05-19 09:38:42 +0700

    Add solution for expected-value-discrete

---
+++ expected-value-discrete/expected-value-discrete.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def expected_value_discrete(x, p):
+    """
+    Returns: float expected value
+    """
+    # Write code here
+    x = np.array(x)
+    p = np.array(p)
+
+    if not np.allclose(np.sum(p), 1):
+        raise ValueError("Probabilities must sum to 1")
+    return np.sum(x * p)
