168ebaee662a93284c314890f7a5c2eb677a63a5
Khoa Thai Minh <149080799+minhkhoa23@users.noreply.github.com>
2026-06-11 11:06:41 +0700

    Solve focal-loss

---
+++ focal-loss/focal-loss.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def focal_loss(p, y, gamma=2.0):
+    """
+    Compute Focal Loss for binary classification.
+    """
+    # Write code here
+    p = np.asarray(p)
+    y = np.asarray(y)
+    np.clip(p, 1e-15, 1 - 1e-15)
+    term1 = (1 - p) ** gamma * y * np.log(p)
+    term2 = p ** gamma * (1 - y) * np.log(1 - p)
+    return np.mean(-(term1 + term2))
