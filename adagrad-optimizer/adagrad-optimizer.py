e55761b264d40947145300a3a5fb5500e987ab72
Khoa Thai Minh <149080799+minhkhoa23@users.noreply.github.com>
2026-07-02 17:57:01 +0700

    Solve adagrad-optimizer

---
+++ adagrad-optimizer/adagrad-optimizer.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def adagrad_step(w, g, G, lr=0.01, eps=1e-8):
+    """
+    Perform one AdaGrad update step.
+    """
+    # Write code here
+    w = np.asarray(w)
+    g = np.asarray(g)
+    G = np.asarray(G)
+
+    new_G = G + g ** 2
+
+    new_w = w - ((lr / np.sqrt(new_G + eps)) * g)
+
+    return (new_w, new_G)
