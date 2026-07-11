04c99113c3f307807fe9c8b56fd669b887819031
Khoa Thai Minh <149080799+minhkhoa23@users.noreply.github.com>
2026-07-11 08:57:44 +0700

    Solve nesterov-momentum

---
+++ nesterov-momentum/nesterov-momentum.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def nesterov_momentum_step(w, v, grad, lr=0.01, momentum=0.9):
+    """
+    Perform one Nesterov Momentum update step.
+    """
+    # Write code here
+    w = np.asarray(w)
+    v = np.asarray(v)
+    grad = np.asarray(grad)
+    w_look = w - momentum * v
+    v_new = momentum * v + lr * grad
+    w_new = w - v_new
+    return (w_new, v_new)
